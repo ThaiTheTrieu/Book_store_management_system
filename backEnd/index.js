492777d38db0edc07ed630dcb3a989bff34936bd
ThaiTheTrieu <thaithetrieu@gmail.com>
2025-06-09 18:51:34 +0700

    conect with reacjs

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,11 +2,23 @@ require('dotenv').config()
 const {env} = require('process');
 const express = require('express');
 const nodemon = require('nodemon');
+const path = require('path');
 
 const app = express();
 
 const port = env.PORT;
 
-app.get('/', (req, res) => res.send("hello world"));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello' });
+});
+
+// app.get('/', (req, res) => res.send("hello world"));
+app.get('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
+});
+
 
 app.listen(port, () => console.log(`server is running on port  ${port}`));
